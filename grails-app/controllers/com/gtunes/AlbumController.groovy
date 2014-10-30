3ead89fac51de72b334a0a7da4cde81d512fa906
Moe B <moeb59@gmail.com>
2014-10-30 17:40:33 -0400

    Static scaffolding completed for Album domain class

--- grails-app/controllers/com/gtunes/AlbumController.groovy
+++ grails-app/controllers/com/gtunes/AlbumController.groovy
@@ -1,5 +1,104 @@
 package com.gtunes
 
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
 class AlbumController {
-    static scaffold = true
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Album.list(params), model:[albumInstanceCount: Album.count()]
+    }
+
+    def show(Album albumInstance) {
+        respond albumInstance
+    }
+
+    def create() {
+        respond new Album(params)
+    }
+
+    @Transactional
+    def save(Album albumInstance) {
+        if (albumInstance == null) {
+            notFound()
+            return
+        }
+
+        if (albumInstance.hasErrors()) {
+            respond albumInstance.errors, view:'create'
+            return
+        }
+
+        albumInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'album.label', default: 'Album'), albumInstance.id])
+                redirect albumInstance
+            }
+            '*' { respond albumInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Album albumInstance) {
+        respond albumInstance
+    }
+
+    @Transactional
+    def update(Album albumInstance) {
+        if (albumInstance == null) {
+            notFound()
+            return
+        }
+
+        if (albumInstance.hasErrors()) {
+            respond albumInstance.errors, view:'edit'
+            return
+        }
+
+        albumInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Album.label', default: 'Album'), albumInstance.id])
+                redirect albumInstance
+            }
+            '*'{ respond albumInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Album albumInstance) {
+
+        if (albumInstance == null) {
+            notFound()
+            return
+        }
+
+        albumInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Album.label', default: 'Album'), albumInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'album.label', default: 'Album'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
--- test/unit/com/gtunes/AlbumControllerSpec.groovy
+++ test/unit/com/gtunes/AlbumControllerSpec.groovy
@@ -1,20 +1,149 @@
 package com.gtunes
 
-import grails.test.mixin.TestFor
-import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
+
+import grails.test.mixin.*
+import spock.lang.*
+
 @TestFor(AlbumController)
+@Mock(Album)
 class AlbumControllerSpec extends Specification {
 
-    def setup() {
+    def populateValidParams(params) {
+        assert params != null
+        // TODO: Populate valid properties like...
+        //params["name"] = 'someValidName'
     }
 
-    def cleanup() {
+    void "Test the index action returns the correct model"() {
+
+        when:"The index action is executed"
+            controller.index()
+
+        then:"The model is correct"
+            !model.albumInstanceList
+            model.albumInstanceCount == 0
     }
 
-    void "test something"() {
+    void "Test the create action returns the correct model"() {
+        when:"The create action is executed"
+            controller.create()
+
+        then:"The model is correctly created"
+            model.albumInstance!= null
+    }
+
+    void "Test the save action correctly persists an instance"() {
+
+        when:"The save action is executed with an invalid instance"
+            request.contentType = FORM_CONTENT_TYPE
+            def album = new Album()
+            album.validate()
+            controller.save(album)
+
+        then:"The create view is rendered again with the correct model"
+            model.albumInstance!= null
+            view == 'create'
+
+        when:"The save action is executed with a valid instance"
+            response.reset()
+            populateValidParams(params)
+            album = new Album(params)
+
+            controller.save(album)
+
+        then:"A redirect is issued to the show action"
+            response.redirectedUrl == '/album/show/1'
+            controller.flash.message != null
+            Album.count() == 1
+    }
+
+    void "Test that the show action returns the correct model"() {
+        when:"The show action is executed with a null domain"
+            controller.show(null)
+
+        then:"A 404 error is returned"
+            response.status == 404
+
+        when:"A domain instance is passed to the show action"
+            populateValidParams(params)
+            def album = new Album(params)
+            controller.show(album)
+
+        then:"A model is populated containing the domain instance"
+            model.albumInstance == album
+    }
+
+    void "Test that the edit action returns the correct model"() {
+        when:"The edit action is executed with a null domain"
+            controller.edit(null)
+
+        then:"A 404 error is returned"
+            response.status == 404
+
+        when:"A domain instance is passed to the edit action"
+            populateValidParams(params)
+            def album = new Album(params)
+            controller.edit(album)
+
+        then:"A model is populated containing the domain instance"
+            model.albumInstance == album
+    }
+
+    void "Test the update action performs an update on a valid domain instance"() {
+        when:"Update is called for a domain instance that doesn't exist"
+            request.contentType = FORM_CONTENT_TYPE
+            controller.update(null)
+
+        then:"A 404 error is returned"
+            response.redirectedUrl == '/album/index'
+            flash.message != null
+
+
+        when:"An invalid domain instance is passed to the update action"
+            response.reset()
+            def album = new Album()
+            album.validate()
+            controller.update(album)
+
+        then:"The edit view is rendered again with the invalid instance"
+            view == 'edit'
+            model.albumInstance == album
+
+        when:"A valid domain instance is passed to the update action"
+            response.reset()
+            populateValidParams(params)
+            album = new Album(params).save(flush: true)
+            controller.update(album)
+
+        then:"A redirect is issues to the show action"
+            response.redirectedUrl == "/album/show/$album.id"
+            flash.message != null
+    }
+
+    void "Test that the delete action deletes an instance if it exists"() {
+        when:"The delete action is called for a null instance"
+            request.contentType = FORM_CONTENT_TYPE
+            controller.delete(null)
+
+        then:"A 404 is returned"
+            response.redirectedUrl == '/album/index'
+            flash.message != null
+
+        when:"A domain instance is created"
+            response.reset()
+            populateValidParams(params)
+            def album = new Album(params).save(flush: true)
+
+        then:"It exists"
+            Album.count() == 1
+
+        when:"The domain instance is passed to the delete action"
+            controller.delete(album)
+
+        then:"The instance is deleted"
+            Album.count() == 0
+            response.redirectedUrl == '/album/index'
+            flash.message != null
     }
 }
