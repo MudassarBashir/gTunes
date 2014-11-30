98d477be2d59bf8b47d7c552e28feb7cf8f855de
Moe B <moeb59@gmail.com>
2014-11-30 16:30:33 -0500

    Created tests for tag library

--- grails-app/taglib/com/gtunes/GtunesTagLib.groovy
+++ grails-app/taglib/com/gtunes/GtunesTagLib.groovy
@@ -2,5 +2,17 @@ package com.gtunes
 
 class GtunesTagLib {
     static defaultEncodeAs = 'html'
+    static namespace = 'gt'
+
     //static encodeAsForTags = [tagName: 'raw']
+
+    def repeat = { attrs, body ->
+        // retrieve the 'times' attribute and convert it to an int
+        int n = attrs.int('times')
+        // render the body 'n' times, passing a 1 based
+        // counter into the body each time
+        n?.times { counter ->
+            out << body(counter + 1)
+        }
+    }
 }
--- test/unit/com/gtunes/GtunesTagLibSpec.groovy
+++ test/unit/com/gtunes/GtunesTagLibSpec.groovy
@@ -15,6 +15,37 @@ class GtunesTagLibSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test repeat tag"() {
+        given:
+            // define a snippet of markup to evaluate
+            def template = '<gt:repeat times="3">Number ${it}</gt:repeat>'
+        when:
+            // evaluate the snippet
+            def result = applyTemplate(template)
+        then:
+            // make sure the result contains what we expect
+            def expected = 'Number 1Number 2Number 3'
+            assert expected == result
+    }
+
+    void "test repeat tag with model"() {
+        given:
+            // define a snippet of markup to evaluate
+            def template =
+                    '<gt:repeat times="${someNumber}">Number ${it}</gt:repeat>'
+        when:
+            // evaluate the snippet
+            def result = applyTemplate(template, [someNumber: 2])
+        then:
+            // make sure the result contains what we expect
+            def expected = 'Number 1Number 2'
+            assert expected == result
+        when:
+        // evaluate the snippet
+        def result2 = applyTemplate(template, [someNumber: 4])
+        then:
+        // make sure the result contains what we expect
+        def expected2 = 'Number 1Number 2Number 3Number 4'
+        assert expected2 == result2
     }
 }
